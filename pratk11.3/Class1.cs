4273e26eecb82ea29ac72a24cbf4ba55c77f0449
undervalued1 <petr1ny06@gmail.com>
2026-02-13 11:04:43 +0500

    init

---
+++ pratk11.3/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pratk11._3
+{
+    class Robot
+    {
+        private int[] hp = new int[3];
+        public int GetLife(int a)
+        {
+            return hp[a];
+        }
+
+        public void AddHp(int a, int b)
+        {
+            hp[b] = a;
+        }
+
+        public void ChangeHp(int a, int b)
+        {
+            hp[b] = hp[b] + a;
+        }
+
+        public void min(int a, int b)
+        {
+            hp[b] = a;
+            hp[b] = (int)hp[b] / 2;
+        }
+    }
+}
